80431214261b6ab286c9e7a2dc12ff12e923a9bf
Mathilda Dembele <mathilda.dembele@gmail.com>
2024-11-28 10:40:51 +0100

    open ai key

--- app/services/chatgpt.py
+++ app/services/chatgpt.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI, Request
 import openai
 from transformers import pipeline
 
-app = FastAPI()
+from dotenv import load_dotenv
+import os
 
-# Configurer les clés d'API
-#openai.api_key = "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"
+load_dotenv()
+
+app = FastAPI()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
