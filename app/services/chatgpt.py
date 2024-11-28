4d5de64754dd2975b1c63ade2541f3adc126877e
Mathilda Dembele <mathilda.dembele@gmail.com>
2024-11-28 10:28:34 +0100

    open ai key

--- app/services/chatgpt.py
+++ app/services/chatgpt.py
@@ -5,4 +5,4 @@ from transformers import pipeline
 app = FastAPI()
 
 # Configurer les clés d'API
-openai.api_key = "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"
+#openai.api_key = "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"
